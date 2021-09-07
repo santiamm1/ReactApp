9318afad654496d306d7ab58059ba8fe999ff37b
Angel Soria <angel@MacBook-Air.local>
2021-09-06 21:06:31 -0300

    react6921

---
+++ react-coder/src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+const ItemListContainer = () => {
+    return (
+        <div className="ItemList">
+            <p>
+                Página en progreso
+            </p>
+        </div>
+    )
+}
+
+export default ItemListContainer
